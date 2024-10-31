8e36fae46799f0d0de55ff6e36cbe5d704865ab9
AryanAngral <pawankumarangral1@gmail.com>
2024-10-31 22:53:24 +0530

    COM-501 python programing assignment done!

---
+++ COM-501-ASSIGNMENT/qu3.py
@@ -0,0 +1,10 @@
+# Input tuple
+t = (-1, 1, 2, 3, 0, 4, 5, 9)
+
+# Find smallest and largest numbers in the tuple
+smallest = min(t)
+largest = max(t)
+
+# Print the results
+print(f"smallest = {smallest}")
+print(f"largest = {largest}")
