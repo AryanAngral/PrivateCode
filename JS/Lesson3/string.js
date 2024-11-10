e1683da8b4e9a341cd6e747e42ad4f3595aa0037
AryanAngral <pawankumarangral1@gmail.com>
2024-11-10 23:48:08 +0530

    web dev course start new

---
+++ JS/Lesson3/string.js
@@ -0,0 +1,47 @@
+'$' + 20.95 + 7.99
+'$20.957.99'
+'$' + (20.95+7.99)
+'$28.939999999999998'
+'$' + ((2095+799)/100)
+'$28.94'
+'Items (' + (1+1) + '): $' + ((2095+799)/100)
+'Items (2): $28.94'
+
+
+
+
+"I'm learning JavaScript"
+"I'm learning JavaScript"
+'I\'m learning JavaScript'
+"I'm learning JavaScript"
+alert('some\n text')
+undefined
+alert('some\ntext')
+undefined
+`Items (${1+1}): $${(2095+799)/100}`
+'Items (2): $28.94'
+
+
+
+
+'My name is:'+'Aryan'
+'My name is:Aryan'
+`Total cost:$${(5+3)}`
+'Total cost:$8'
+'Total cost:$'+(5+3)
+'Total cost:$8'
+alert('Total cost:$'+(5+3))
+undefined
+`Items (${(2+2)}): $${((2*2095)+(2*799))/100}`
+'Items (4): $57.88'
+
+
+((2095*2 +799*2)+499+499)/100
+67.86
+`SHIPPING % HANDLING: $${(499+499)/100}`
+'SHIPPING % HANDLING: $9.98'
+`Total before tax: $${((2095*2 +799*2)+499+499)/100}`
+'Total before tax: $67.86'
+
+`Estimated tax (10%): $${Math.round(((2095*2 +799*2)+499+499)*0.1)/100}`
+'Estimated tax (10%): $6.79'
